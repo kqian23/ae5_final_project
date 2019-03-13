219a550e6854b7ab20061717aa4760122fa480f5
Brandon Twilley <btwilley@uw.edu>
2019-03-12 21:23:48 -0700

    Add Summary of data

--- ui.R
+++ ui.R
@@ -39,8 +39,8 @@ question_one_page <- tabPanel("Poverty & Gender of Smokers")
 question_two_page <- tabPanel("Poverty & Underage Smoking",
                               sidebarLayout(
                                 sidebarPanel(
-                                  # A `titlePanel` with the title "Poverty and Youth Tobacco by State"
-                                  titlePanel("Poverty and Youth Tobacco by State"),
+                                  # A `titlePanel` with the title "Poverty and Youth Tobacco Usage by Year"
+                                  titlePanel("Poverty and Youth Tobacco Usage by Year"),
                                   
                                   # A `selectInput()` labeled "Select a state". This dropdown should let
                                   # the user pick one of the states in the joined data set.
@@ -49,7 +49,20 @@ question_two_page <- tabPanel("Poverty & Underage Smoking",
                                   
                                 ),
                                 mainPanel(
-                                  plotOutput(outputId = "my_plot")
+                                  plotOutput(outputId = "my_plot"),
+                                      h2("Is there causation between youth poverty 
+                                         and under age smoking?"),
+                                      h3("By joining the youth tobacco dataset and 
+                                      the youth poverty dataset we created a new 
+                                      dataset observing the state, year, 
+                                      0-17-year-old poverty rate, and underage 
+                                      smoking. The visualization allows the user 
+                                      to choose a year and see if there is 
+                                      causation with the youth poverty rate and 
+                                      underage smoking of that year. According 
+                                      to the data there is no causation between 
+                                      youth poverty rate and underage smoking, 
+                                      but there is moderate correlation.")
                                 )
                               ))
 
