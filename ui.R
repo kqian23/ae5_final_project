a378d71b01b3f50ea27af86e41b9a01e01f63cf4
Brandon Twilley <btwilley@uw.edu>
2019-03-12 19:16:02 -0700

    Question-two UI changes

--- ui.R
+++ ui.R
@@ -1,35 +1,40 @@
 library("shiny")
 source("server.R")
 
-home_page <- tabPanel("Home",
-            #  includeCSS("www/header.css"),
-              includeHTML("www/header.html"),
-              br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),
-              titlePanel(h1("Study on Youth Poverty and Youth Smoking", align = "center", style = "font-size: 70px"))
+home_page <- tabPanel(
+  "Home",
+  #  includeCSS("www/header.css"),
+  includeHTML("www/header.html"),
+  br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), 
+  br(), br(), br(), br(),
+  titlePanel(h1("Study on Youth Poverty and Youth Smoking", align = "center", 
+                style = "font-size: 70px"))
 )
 
-team_page <- tabPanel("Team",
-             # includeCSS("www/header.css"),
-              includeHTML("www/header.html"),
-              br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),
-              titlePanel(h1("Our Team", align = "left", style = "font-size: 70px")),
-              br(), br(),
-              includeCSS("www/profile.css"),
-              includeHTML("www/profile.html")
-              
+team_page <- tabPanel(
+  "Team",
+  # includeCSS("www/header.css"),
+  includeHTML("www/header.html"),
+  br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), 
+  br(), br(), br(), br(),
+  titlePanel(h1("Our Team", align = "left", style = "font-size: 70px")),
+  br(), br(),
+  includeCSS("www/profile.css"),
+  includeHTML("www/profile.html")
 )
 
-sources_page <- tabPanel("Sources",
-                 includeHTML("www/header.html"),
-                 br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(), br(),
-                 includeHTML("www/sources.html")
+sources_page <- tabPanel(
+  "Sources",
+  includeHTML("www/header.html"),
+  br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), 
+  br(), br(), br(), br(), br(), br(),
+  includeHTML("www/sources.html")
 )
 
 
 
 
 question_one_page <- tabPanel("Poverty & Gender of Smokers")
-
 
 question_two_page <- tabPanel("Poverty & Underage Smoking",
                               sidebarLayout(
@@ -48,48 +53,87 @@ question_two_page <- tabPanel("Poverty & Underage Smoking",
                                 )
                               ))
 
- 
 
-question_four_page <- tabPanel("Question 4",
-                           sidebarLayout(
-                             sidebarPanel(
-                               selectInput(
-                                 inputId = "year",
-                                 label = "Select a year between 1992 and 2016",
-                                 choices = list("1999", "2000", "2001", "2002", "2003",
-                                                "2004", "2005", "2006", "2007", "2008",
-                                                "2009", "2010", "2011", "2012","2013",
-                                                "2014","2015", "2016", "2017")
-                               ),
-                               radioButtons(
-                                 inputId = "feature",
-                                 label = "Select a feature of interest to compare against",
-                                 choices = list (
-                                   "Frequent",
-                                   "Ever",
-                                   "Current")
-                               )
-                             ),
-                             mainPanel(
-                                plotOutput(
-                                  outputId = "education"
-                                ) 
-                              )
-                            )
-                          )
+
+question_three_page <- tabPanel(
+  "Poverty and Smoking Cessation",
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "years",
+        label = "Select a year between 1992 and 2017",
+        choices = list(
+          "1999", "2000", "2001", "2002", "2003",
+          "2004", "2005", "2006", "2007", "2008",
+          "2009", "2010", "2011", "2012", "2013",
+          "2014", "2015", "2016", "2017"
+        )
+      ),
+      radioButtons(
+        inputId = "gender",
+        label = "Select a gender of interest to compare against",
+        choices = list(
+          "Male",
+          "Female",
+          "Overall"
+        )
+      )
+    ),
+    mainPanel(
+      plotOutput(
+        outputId = "cessation"
+      )
+    )
+  )
+)
+
+
+question_four_page <- tabPanel(
+  "Question 4",
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "year",
+        label = "Select a year between 1992 and 2016",
+        choices = list(
+          "1999", "2000", "2001", "2002", "2003",
+          "2004", "2005", "2006", "2007", "2008",
+          "2009", "2010", "2011", "2012", "2013",
+          "2014", "2015", "2016", "2017"
+        )
+      ),
+      radioButtons(
+        inputId = "feature",
+        label = "Select a feature of interest to compare against",
+        choices = list(
+          "Frequent",
+          "Ever",
+          "Current"
+        )
+      )
+    ),
+    mainPanel(
+      plotOutput(
+        outputId = "education"
+      )
+    )
+  )
+)
+
+
 ui <- fluidPage(
   includeCSS("www/bootstrap.css"),
   includeCSS("www/header.css"),
-  navbarPage("",
+  navbarPage(
+    "",
     home_page,
-     question_one_page,
-     question_two_page,
-     tabPanel("Q3"
-              # Q3 Code starts here
-     ),
+    question_one_page,
+    question_two_page,
+    tabPanel(
+    ),
+    question_three_page,
     question_four_page,
     team_page,
     sources_page
   )
 )
-
